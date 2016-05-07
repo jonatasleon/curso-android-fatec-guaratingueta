028a26e32982bc74728c0e5bae0a65dd6ce8f11f
Jonatas Leon <jonatas.leon@gmail.com>
2016-05-07 18:32:49 -0300

    Aula do dia 7/5/2016

--- AtividadeII/app/src/main/java/com/jonatasleon/atividadeii/MainActivity.java
+++ AtividadeII/app/src/main/java/com/jonatasleon/atividadeii/MainActivity.java
@@ -7,39 +7,59 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText edtN1;
+    private EditText edtN2;
+
+    private TextView tvResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ((Button) findViewById(R.id.soma)).setOnClickListener(this);
-        ((Button) findViewById(R.id.subtracao)).setOnClickListener(this);
-        ((Button) findViewById(R.id.multiplicacao)).setOnClickListener(this);
-        ((Button) findViewById(R.id.divisao)).setOnClickListener(this);
+
+        edtN1 = (EditText) findViewById(R.id.edt_n1);
+        edtN2 = (EditText) findViewById(R.id.edt_n2);
+
+        tvResultado = (TextView) findViewById(R.id.tv_resultado);
+
+        Button btnSoma = (Button) findViewById(R.id.btn_soma);
+        btnSoma.setOnClickListener(this);
+
+        Button btnSubtracao = (Button) findViewById(R.id.btn_subtracao);
+        btnSubtracao.setOnClickListener(this);
+
+        Button btnMultiplicacao = (Button) findViewById(R.id.btn_multiplicacao);
+        btnMultiplicacao.setOnClickListener(this);
+
+        Button btnDivisao = (Button) findViewById(R.id.btn_divisao);
+        btnDivisao.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        int n1 = Integer.parseInt(((EditText) findViewById(R.id.n1)).getText().toString());
-        int n2 = Integer.parseInt(((EditText) findViewById(R.id.n2)).getText().toString());
-        double res = 0;
+        double resultado = 0.0;
+        int n1, n2;
+        n1 = Integer.parseInt(edtN1.getText().toString());
+        n2 = Integer.parseInt(edtN2.getText().toString());
 
-        switch (v.getId()){
-            case R.id.soma:
-                res = n1 + n2;
-                break;
-            case R.id.subtracao:
-                res = n1 - n2;
-                break;
-            case R.id.multiplicacao:
-                res = n1 * n2;
-                break;
-            case R.id.divisao:
-                res = n1 / n2;
-                break;
+        int id = v.getId();
+
+        if(id == R.id.btn_soma) {
+            resultado = n1 + n2;
+        } else if (id == R.id.btn_subtracao) {
+            resultado = n1 - n2;
+        } else if (id == R.id.btn_multiplicacao) {
+            resultado = n1 * n2;
+        } else if (id == R.id.btn_divisao) {
+            if(n2 == 0) {
+                tvResultado.setText(R.string.aviso_numero_invalido);
+                return;
+            }
+            resultado = n1 / n2;
         }
 
-        ((TextView) findViewById(R.id.res)).setText(String.valueOf(res));
+        tvResultado.setText(String.valueOf(resultado));
     }
 }
